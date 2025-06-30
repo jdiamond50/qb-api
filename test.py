189de1da9d1d596b9148089a5b81f3cca70ee3d7
Julien Diamond <jdiamond50@stuy.edu>
2025-06-30 18:19:56 -0400

    successful test of api requests

---
+++ test.py
@@ -0,0 +1,18 @@
+import requests # used to make api request
+import json # used to parse api request
+
+# python3 -m venv qb-venv
+# source qb-venv/bin/activate
+# python3 -m pip install requests
+
+print("HI")
+
+response = requests.get("http://api.open-notify.org/astros")
+print(response.status_code)
+print(response.json())
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=False, indent=4)
+    print(text)
+
+jprint(response.json())
