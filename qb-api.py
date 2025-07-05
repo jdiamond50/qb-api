5943c11a4d300ad6382cc775ae4492731dbe8237
Julien Diamond <juliendiamond@MacBook-Pro.local>
2025-07-05 15:17:24 -0400

    added parameters to api request for tossup of specified difficulties

--- qb-api.py
+++ qb-api.py
@@ -1,16 +1,34 @@
 import requests
 import json
 
-response = requests.get("https://www.qbreader.org/api/random-tossup")
-print(response.status_code)
-
 def jprint(obj):
     text = json.dumps(obj, sort_keys=False, indent=4)
     print(text)
 
-tossup = response.json()["tossups"][0]
+# random tossup
+print("------------ RANDOM TOSSUP ------------")
+response = requests.get("https://www.qbreader.org/api/random-tossup")
+if (response.status_code == 200):
+    tossup = response.json()["tossups"][0]
 
-jprint(tossup)
+    # print("------- API RESPONSE JSON -------")
+    # jprint(tossup) # uncomment to see available info about tossup
 
-print(tossup["question_sanitized"])
-print(tossup["answer_sanitized"])
+    print("------- TOSSUP -------")
+    print(tossup["question_sanitized"])
+    print(tossup["answer_sanitized"])
+
+# random tossup difficulties 4-5
+print("------------ TOSSUP DIFFS 4-5 ------------")
+diffs = [4,5]
+params = {"difficulties" : diffs}
+response = requests.get("https://www.qbreader.org/api/random-tossup", params = params)
+if (response.status_code == 200):
+    tossup = response.json()["tossups"][0]
+
+    print("------- API RESPONSE JSON -------")
+    jprint(tossup)
+
+    print("------- TOSSUP -------")
+    print(tossup["question_sanitized"])
+    print(tossup["answer_sanitized"])
